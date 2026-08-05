6aa3cef5dd94a221f3672e2667e850cbaede93cf
NH-Bappy <hnaimul302@gmail.com>
2026-08-05 19:28:18 +0600

    style(projects): set project card and modal images to object-cover

--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -69,7 +69,7 @@ export default function ProjectModal({ project, onClose }) {
             <img
               src={activeImage || project.image}
               alt={project.title}
-              className="w-full h-full object-contain bg-slate-950 p-1"
+              className="w-full h-full object-cover"
             />
           </div>
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -83,7 +83,7 @@ export default function Projects({ onSelectProject }) {
                     <img
                       src={project.image}
                       alt={project.title}
-                      className="w-full h-full object-contain bg-slate-950 transition-transform duration-700 group-hover:scale-105 p-1"
+                      className="w-full h-full object-cover transition-transform duration-700 group-hover:scale-105"
                     />
 
                     {/* Hover Overlay Button */}
